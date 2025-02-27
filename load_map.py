cabf33d70accc8f717ecb2b4660b718fd9410767
Mickaël <mickael.debraekeleer@student.unamur.be>
2025-02-27 11:36:27 +0100

    Update load_map.py

--- load_map.py
+++ load_map.py
@@ -1,4 +1,3 @@
-import os
 def load_map(map_file_path:str)->dict:
     """Load the map files and return their data in the form of a dictionnary
     
@@ -13,11 +12,10 @@ def load_map(map_file_path:str)->dict:
     Version
     -------
     specification: Mitta Kylian, De Braekeleer Mickaël (v.1 20/02/25)
+    implementation: De Braekeleer Mickaël (v1 26/02/25)
     """
     #init game data dict with basic info
-    game_data={"player1": {"call" : 10},
-               "player2": {"call" : 10}
-               }
+    game_data={"map":None, "player1":{"call":10, "apprentices":{}, "dragon":{}}, "player2":{"call":10, "apprentices":{}, "dragon":{}}, "eggs":{}}
     #open the file of the map
     map=open(map_file_path, "r")
     #get all data lines inside a list (list of lines)
@@ -27,37 +25,43 @@ def load_map(map_file_path:str)->dict:
     
     #search in all lines for data
     for line in range (len(raw_data)):
-        if raw_data[line] =="map:":
-            game_data["map"]=raw_data[line+1].split(" ")
-        elif raw_data[line] == "apprentices:":
+        print(raw_data[line])
+        if raw_data[line].split(":")[0] =="map":
+            info=raw_data[line+1].split(" ")
+            game_data["map"]=[int(info[0]), int(info[1])]
+        elif raw_data[line].split(":")[0] == "apprentices":
             line+=1
-            while raw_data[line] !="eggs:":
+            while raw_data[line].split(":")[0] !="eggs":
                 info=raw_data[line].split(" ")
                 if info[0]==1:
-                    game_data["player1"]["apprentices"][info[1]]["pos"]=[info[2], info[3]]
-                    game_data["player1"]["apprentices"][info[1]]["max_health"]=info[5]
-                    game_data["player1"]["apprentices"][info[1]]["current_health"]=info[5]
-                    game_data["player1"]["apprentices"][info[1]]["regeneration"]=info[6]
+                    game_data["player1"]["apprentices"][info[1]]={}
+                    game_data["player1"]["apprentices"][info[1]]["pos"]=[int(info[2]), int(info[3])]
+                    game_data["player1"]["apprentices"][info[1]]["max_health"]=int(info[4])
+                    game_data["player1"]["apprentices"][info[1]]["current_health"]=int(info[4])
+                    game_data["player1"]["apprentices"][info[1]]["regeneration"]=int(info[5][0:1])
                 else:
-                    game_data["player2"]["apprentices"][info[1]]["pos"]=[info[2], info[3]]
-                    game_data["player2"]["apprentices"][info[1]]["max_health"]=info[5]
-                    game_data["player2"]["apprentices"][info[1]]["current_health"]=info[5]
-                    game_data["player2"]["apprentices"][info[1]]["regeneration"]=info[6]          
+                    game_data["player2"]["apprentices"][info[1]]={}
+                    game_data["player2"]["apprentices"][info[1]]["pos"]=[int(info[2]), int(info[3])]
+                    game_data["player2"]["apprentices"][info[1]]["max_health"]=int(info[4])
+                    game_data["player2"]["apprentices"][info[1]]["current_health"]=int(info[4])
+                    game_data["player2"]["apprentices"][info[1]]["regeneration"]=int(info[5][0:1])      
                 line+=1
-        elif raw_data[line]=="eggs:":
+        elif raw_data[line].split(":")[0]=="eggs":
             for eggs in range (len(raw_data)-line):
                 line+=1
-                egg=raw_data[line].split(" ")
-                game_data["eggs"][egg[0]]["pos"]=[egg[1], egg[2]]
-                game_data["eggs"][egg[0]]["time_to_hatch"]=[egg[1], egg[3]]
-                game_data["eggs"][egg[0]]["max_health"]=egg[4]
-                game_data["eggs"][egg[0]]["current_health"]=egg[4]
-                game_data["eggs"][egg[0]]["attack_damage"]=egg[5]
-                game_data["eggs"][egg[0]]["attack_range"]=egg[6]
-                game_data["eggs"][egg[0]]["regeneration"]=egg[7]
-                  
-            return game_data
-    
+                info=raw_data[line].split(" ")
+                game_data["eggs"][info[0]]={}
+                game_data["eggs"][info[0]]["pos"]=[int(info[1]), int(info[2])]
+                game_data["eggs"][info[0]]["time_to_hatch"]=int(info[3])
+                game_data["eggs"][info[0]]["max_health"]=int(info[4])
+                game_data["eggs"][info[0]]["current_health"]=int(info[4])
+                game_data["eggs"][info[0]]["attack_damage"]=int(info[5])
+                game_data["eggs"][info[0]]["attack_range"]=int(info[6])
+                game_data["eggs"][info[0]]["regeneration"]=int(info[7][0:1])
+                if line == len(raw_data)-1:  
+                    return game_data
+
+#example   
 map_path="C:/Users/coram/OneDrive/Desktop/projet/map.drk" 
 game_data=load_map(map_path)
 print(game_data)
