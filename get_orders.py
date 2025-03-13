ef7b7caff6f7bfda3c3c4c582a12c30407cf75d4
Mickaël <mickael.debraekeleer@student.unamur.be>
2025-03-13 10:54:11 +0100

    Update get_orders.py

--- get_orders.py
+++ get_orders.py
@@ -1,52 +1,47 @@
-def Get_orders(game_data:dict) -> list:
+def Get_orders(player ,game_data:dict) -> list:
     """Ask the player what he want to play
 
     parameters
     ----------
+    player : name of the player (str)
     game_data : dictionnary of dictionnary that contain all game data about player and the map(dict)
     
     return
     ------
-    orders ; list of order with the correct format (list)
+    orders_player : order with the correct format (list)
+    
     Version
     -------
     specification: Mitta Kylian, De Braekeleer Mickaël (v.1 20/02/25)
     implementation: Mitta Kylian (v1 03/03/25)
     
     """
-    orders_player_1=[]
-    orders_player_2=[]
+    orders_player=[]
 
-
-    list_player=orders_player_1
-    player="player1"
 
     for temp in range(2):
 
-        orders=input("ordres ->")
+        orders=input(f"{player}, veuillez entrer votre ordre ->")
 
         order=orders.split(" ")
         for order in orders:
              
-                  if f"{player}summon" in order:
-                      list_player.append(order)
+            if "summon" in order:
+                orders_player.append(order)
                       
-                  elif ':' in order:
-                        check=order.split(":")
+            elif ':' in order:
+                check=order.split(":")
 
-                        if (check[1][0]=="@" 
-                        and check[1][1].isdigit() 
-                        and check[1][2]=="-" 
-                        and check[1][3].isdigit()
-                        and check[0] in game_data[player]["apprentices"]):
-                                 list_player.append(order)
+                if (check[1][0]=="@" 
+                and check[1][1].isdigit() 
+                and check[1][2]=="-" 
+                and check[1][3].isdigit()
+                and check[0] in game_data[player]["apprentices"]):
+                    orders_player.append(order)
                  
-                        elif check[0] in game_data[player]["dragon"]:
+                elif check[0] in game_data[player]["dragon"]:
 
-                            if check[1] in {"xN","xNE","xE","xSE","xS","xSW","xW","xNW"}:
-                                 list_player.append(order)
-        list_player=orders_player_2
-        player="player2"
+                    if check[1] in {"xN","xNE","xE","xSE","xS","xSW","xW","xNW"}:
+                        orders_player.append(order)
         
-
-    return  orders_player_1,orders_player_2
+    return  orders_player
