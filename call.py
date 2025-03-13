2b30e9e12741cad45cdffeb23f4b9a81acc1a79c
Hamizzatcapalot <hamizzatsosa98@gmail.com>
2025-03-13 13:07:43 +0000

    Add files via upload

--- call.py
+++ call.py
@@ -1,4 +1,5 @@
 def call(game_data):
+    
     """The function to use the special ability of the player
 
     Parameters
@@ -13,21 +14,40 @@ def call(game_data):
     -------
     specification: Hamza SOSSEY-ALAOUI (v.1 20/02/25)
     """
-    altars1=game_data['player1']['altars'] 
-    posof_app1=game_data['player1']['apprentices']['pos']
-    posof_drag1=game_data['player1']['dragon']['pos']
-    posof_AppDrag1={**posof_app1,**posof_drag1}
-    for pos in posof_AppDrag1:
-        pos[0]=altars1[0] and pos[1]=altars1[1]
+    for order in get_orders('player1',game_data):
+        if 'summon' in order:
+            #get position of where dragons and apprentices should go when the call
+            altars1=game_data['player1']['altars']
+            #get all the dragons and apprentices  
+            all_app1=game_data['player1']['apprentices']
+            all_drag1=game_data['player1']['dragon']
+            posof_AppDrag1=[]
+            for apprentice in all_app1:
+                posof_app=apprentice['pos']
+                posof_AppDrag1.append(posof_app)
+            for dragon in all_drag1:
+                posof_drag=dragon['pos']
+                posof_AppDrag1.append(posof_drag)
+            for pos in posof_AppDrag1:
+                pos[0]=altars1[0] and pos[1]=altars1[1]
         
-    
-    altars2=game_data['player2']['altars']
-    posof_app2=game_data['player2']['apprentices']['pos']
-    posof_drag2=game_data['player2']['dragon']['pos']
-    posof_AppDrag2={**posof_app2,**posof_drag2}
-    for pos in posof_AppDrag2:
-        pos[0]=altars2[0] and pos[1]=altars2[1]
-        
+    for order in get_orders('player2',game_data):
+        if 'summon' in order:
+            altars2=game_data['player2']['altars']
+            #get all the dragons and apprentices  
+            all_app2=game_data['player2']['apprentices']
+            all_drag2=game_data['player2']['dragon']
+            posof_AppDrag2=[]
+            for apprentice in all_app2:
+                posof_app=apprentice['pos']
+                posof_AppDrag2.append(posof_app)
+            for dragon in all_drag2:
+                posof_drag=dragon['pos']
+                posof_AppDrag2.append(posof_drag)
+            for pos in posof_AppDrag2:
+                pos[0]=altars2[0] and pos[1]=altars2[1]
+
     display(game_data)
     return game_data    
     
+
