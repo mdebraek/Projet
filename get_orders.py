b242bfb6888f9c85901adf27e934942d3bdae9c9
Mickaël <mickael.debraekeleer@student.unamur.be>
2025-03-13 11:16:53 +0100

    Update get_orders.py

--- get_orders.py
+++ get_orders.py
@@ -18,30 +18,34 @@ def Get_orders(player ,game_data:dict) -> list:
     """
     orders_player=[]
 
+    orders=input(f"{player}, veuillez entrer votre ordre ->")
 
-    for temp in range(2):
-
-        orders=input(f"{player}, veuillez entrer votre ordre ->")
-
-        order=orders.split(" ")
-        for order in orders:
-             
-            if "summon" in order:
-                orders_player.append(order)
+    order=orders.split(" ")
+    for order in orders:
+         
+        if "summon" in order:
+            orders_player.append(order)
                       
-            elif ':' in order:
-                check=order.split(":")
+        elif ':' in order:
+            check=order.split(":")
 
-                if (check[1][0]=="@" 
-                and check[1][1].isdigit() 
-                and check[1][2]=="-" 
-                and check[1][3].isdigit()
-                and check[0] in game_data[player]["apprentices"]):
-                    orders_player.append(order)
+            if (check[1][0]=="@" 
+            and check[1][1].isdigit() 
+            and check[1][2]=="-" 
+            and check[1][3].isdigit()
+            and check[0] in game_data[player]["apprentices"]):
+                orders_player.append(order)
                  
-                elif check[0] in game_data[player]["dragon"]:
-
-                    if check[1] in {"xN","xNE","xE","xSE","xS","xSW","xW","xNW"}:
-                        orders_player.append(order)
+            
+                if check[0] in game_data[player]["dragon"]:
+                   if (check[1][0]=="@" 
+                    and check[1][1].isdigit() 
+                    and check[1][2]=="-" 
+                    and check[1][3].isdigit()
+                    and check[0] in game_data[player]["dragon"]):
+                       orders_player.append(order)
+                       
+                   elif check[1] in {"xN","xNE","xE","xSE","xS","xSW","xW","xNW"}:
+                       orders_player.append(order)
         
     return  orders_player
